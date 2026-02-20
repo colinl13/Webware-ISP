bcd750515142cd740084ad96d53bc204e0a7835e
colinl13 <clemire@wpi.edu>
2026-02-20 14:51:26 -0500

    added functionality to level menu and made it look slightly better; need to refactor js for all routes

---
+++ public/levelselect.js
@@ -0,0 +1,20 @@
+const level_menu = document.getElementById("level_menu");
+const level_one = document.getElementById("to_level_one");
+const level_two = document.getElementById("to_level_two");
+const level_three = document.getElementById("to_level_three");
+const login_button = document.getElementById("login_button");
+const main_menu = document.getElementById("main_menu");
+
+level_one.addEventListener("click", () =>{
+    window.location.href = "/levelone";
+})
+
+level_two.addEventListener("click", () =>{
+    window.location.href = "/leveltwo";
+})
+
+// level_three.addEventListener("click", () =>{
+//     window.location.href = "/levelthree";
+// })
+
+
