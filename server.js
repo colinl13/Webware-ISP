88785c16fcc78682604ff7b618f28c9287039991
Colin <colinlemire14@gmail.com>
2026-03-05 17:08:20 -0500

    fix base_url for deployment

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.SECRET,
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.BASE_URL || 'http://localhost:3000',
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL
 };
